c3ebe4c5b8df0fb48f375ad2e782474f3c950052
asuraCoder <asuracoder@gmail.com>
2022-08-28 16:14:48 +0900

    done

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,22 +1,85 @@
 import gulp from "gulp";
 import gpug from "gulp-pug";
-//import del from "del";
+import del from "del";
+import ws from "gulp-webserver";
+import image from "gulp-image";
+import autop from "gulp-autoprefixer";
+import miniCSS from "gulp-csso";
+
+import dartSass from "sass";
+import gulpSass from "gulp-sass";
+const sass = gulpSass(dartSass);
+
+import bro from "gulp-bro";
+import babelify from "babelify";
 
 const routes = {
 	pug: {
+		watch: "src/**/*.pug",
 		src: "src/*.pug",
-		// src폴더안의 다른 폴더안에 있는 pug까지 컴파일 하려면 /src/**/*.pug라고 쓰면됨
 		dest: "build",
+	},
+	img: {
+		src: "src/img/*",
+		dest: "build/img",
+	},
+	scss: {
+		watch: "src/scss/**/*.scss",
+		src: "src/scss/style.scss",
+		dest: "build/css",
+	},
+	js: {
+		watch: "src/js/**/*.js",
+		src: "src/js/main.js",
+		dest: "build/js",
 	},
 };
 
-export function pug() {
+function pug() {
 	return gulp
 		.src(routes.pug.src)
 		.pipe(gpug())
 		.pipe(gulp.dest(routes.pug.dest));
 }
 
-//export const clean = () => del(["build"]);
+const clean = () => del(["build"]);
 
-export const dev = gulp.series([pug]);
+const webserver = () =>
+	gulp.src("build").pipe(ws({ livereload: true, open: true }));
+
+const js = () =>
+	gulp
+		.src(routes.js.src)
+		.pipe(
+			bro({
+				transform: [
+					babelify.configure({ presets: ["@babel/preset-env"] }),
+					["uglifyify", { global: true }],
+				],
+			})
+		)
+		.pipe(gulp.dest(routes.js.dest));
+
+const styles = () =>
+	gulp
+		.src(routes.scss.src)
+		.pipe(sass().on("error", sass.logError))
+		.pipe(autop())
+		.pipe(miniCSS())
+		.pipe(gulp.dest(routes.scss.dest));
+
+const img = () =>
+	gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dest));
+
+const watch = () => {
+	gulp.watch(routes.pug.watch, pug);
+	gulp.watch(routes.img.src, img);
+	gulp.watch(routes.scss.watch, styles);
+	gulp.watch(routes.js.watch, js);
+};
+
+const prepare = gulp.series([clean, img]);
+const assets = gulp.series([pug, styles, js]);
+const postDev = gulp.parallel([webserver, watch]);
+
+export const dev = gulp.series([prepare, assets, postDev]);
